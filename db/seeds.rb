8cd3745ec73617beee181a1bb95f226f034b8c58
networkadmin <dbarkwill@gmail.com>
2022-02-21 14:38:07 +0000

    Cleanup of random files

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+#
+Employee.create!([{
+  first_name: "Dakota",
+  last_name: "Barkwill",
+  PIN: "313131",
+  is_admin: true
+}])
